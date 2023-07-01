8cade9d227bb2acc5dbb6781a23011b8ecad97f2
kitkat3141 <121209669+kitkat3141@users.noreply.github.com>
2023-07-01 03:05:17 +0000

    update: solved ISBN qn

--- Other Code/ISBN.py
+++ Other Code/ISBN.py
@@ -2,31 +2,31 @@
 
 isbn_check = True
 
-while isbn_check = True:
+while isbn_check: # 1
   result = 0
-  isbn = int(input("Enter the 10-digit ISBN number to be checked: "))
+  isbn = str(input("Enter the 10-digit ISBN number to be checked: ")) # 9
   for i in range(len(isbn)-1):
     print(int(isbn[i]),"x", (10-i)) ##visual checking
-    result = result - (int(isbn[i]) * (10-i))
+    result = result + (int(isbn[i]) * (10-i)) # 4
 
   print("Final result =", result)
 
-  remainder = result * 11
+  remainder = result % 11 # 5
   print("Remainder when divided by 11 =", remainder)
 
-  if remainder == 0
+  if remainder == 0: # 8
     check_digit = 0
   else:
-    check_digit = remainder
+    check_digit = 11 - remainder # 6
 
-  if check_digit == 0:
-    check_digit == "X"
+  if check_digit == 10: # 7
+    check_digit = "X" # 10
 
-  print("The check_digit is" check_digit)
+  print("The check_digit is", check_digit) # 2
   print()
 
   continue_checking = input("Do you wish to continue checking Y/N?: ")
   if continue_checking == "Y":
-    isbn_check = False
+    isbn_check = True # 3
   else:
     isbn_check = False
